48f7a6d58a8d63d8b017cd3df4dfcbbf8e8be02e
lady.d3v1l <melissamarquespt@hotmail.com>
2025-09-23 14:58:38 +0100

    nasceu

---
+++ philo/philosophers.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jobraga- <jobraga-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/22 15:31:57 by jobraga-          #+#    #+#             */
+/*   Updated: 2025/09/23 13:35:38 by jobraga-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#define PHILOSOPHERS_H
+# ifndef PHISOSOPHERS_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+
+
+#endif
