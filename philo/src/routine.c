637c1664ef5114511225f22c9f6b6de8aef0148d
lady.d3v1l <melissamarquespt@hotmail.com>
2025-10-22 11:43:18 +0100

    acho que foi em

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: jobraga- <jobraga-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 16:13:41 by jobraga-          #+#    #+#             */
-/*   Updated: 2025/10/20 17:54:05 by jobraga-         ###   ########.fr       */
+/*   Updated: 2025/10/22 11:38:51 by jobraga-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,8 @@ void	take_forks(t_philo *philo, int id)
 
 void	philo_eat(t_philo *philo)
 {
-	pthread_mutex_lock(&philo->data->mutex.meal_flag);
 	take_forks(philo, philo->id);
+	pthread_mutex_lock(&philo->data->mutex.meal_flag);
 	philo->last_eat_time = ft_get_time();
 	philo->count_eat++;
 	if (philo->count_eat == philo->data->args.count_eat)
